949afceab3d84a49220df38d9cfd409c417f3b0f
ZhizhenZhou <zzz20001205@gmail.com>
2024-09-03 17:10:35 +0800

    Fix: fix error when accessing the system clipboard in WSL.

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,18 @@
+if vim.fn.has("wsl") == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
+
 vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
