0e11d7a580518a939cdc791b3777c53fd5e45689
ZhizhenZhou <53110831+ZhizhenZhou@users.noreply.github.com>
2024-12-16 13:17:25 +0800

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,7 +9,7 @@ vim.opt.softtabstop = 4 -- number of spacesin tab when editing
 vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
 
-vim.opt.smartindent = true 
+vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
